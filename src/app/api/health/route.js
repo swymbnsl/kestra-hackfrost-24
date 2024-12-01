01f3fec73c18e9b74f5da2018bf3593d52656173
prathamjagga <prathamjagga123@gmail.com>
2024-12-01 18:45:26 +0530

    add health api

---
+++ src/app/api/health/route.js
@@ -0,0 +1,11 @@
+export async function GET(request) {
+  try {
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching issues:", error);
+    return NextResponse.json(
+      { success: false, message: "Some services not working" },
+      { status: 500 }
+    );
+  }
+}
